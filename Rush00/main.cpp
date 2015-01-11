5938a25e9c1ae90b570e1f2572b0f704384cb1ec
tettouat <tettouat@sutdent.42.fr>
2015-01-11 16:19:24 +0100

    condition de win

--- Rush00/main.cpp
+++ Rush00/main.cpp
@@ -4,14 +4,14 @@ int main() {
     WindowRetro *win = new WindowRetro();
 
     win->addAliens(5, 2, 1, 150);
-    win->addAliens(20, 2, 1, 200);
-    win->addAliens(35, 2, 1, 300);
-    win->addAliens(50, 2, 1, 300);
+    win->addAliens(25, 2, 1, 200);
+    win->addAliens(45, 2, 1, 300);
     win->addAliens(65, 2, 1, 300);
-    win->addAliens(80, 2, 1, 300);
-    win->addAliens(95, 2, 1, 300);
-    win->addAliens(110, 2, 1, 300);
-    win->addAliens(125, 2, 1, 300);
+    win->addAliens(85, 2, 1, 300);
+    win->addAliens(15, 5, 1, 300);
+    win->addAliens(35, 5, 1, 300);
+    win->addAliens(55, 5, 1, 300);
+    win->addAliens(75, 5, 1, 300);
 
     win->Play();
     delete win;
